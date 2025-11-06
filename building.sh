dca6f534d6957d359ac5e0358d04d1e8e56603f6
naiqixiao <naiqi.xiao@kangleelab.com>
2025-11-05 20:57:27 -0500

    Add Anywhy Flake Shield configuration and keymap
    - Created keymap for Anywhy Flake shield with various layers and combos.
    - Added YAML configuration file for the Anywhy Flake shield.
    - Implemented left and right overlay files for key scanning.
    - Introduced combos configuration for key combinations and actions.
    - Removed unused zmk-helpers files to streamline the project.
    - Added a Docker build script for easier firmware compilation.
    - Deleted obsolete module.yml file related to Zephyr build settings.

--- building.sh
+++ building.sh
@@ -4,12 +4,23 @@ export ZMK_CONFIG=/Users/naiqixiao/Documents/GitHub/flake-zmk-module/config
 cd $ZMK_LOCAL
 source .venv/bin/activate  
 
+west build -s zmk/app -d "/tmp/tmp.ldrWldLPWN" -b "seeeduino_xiao_ble" -S "studio-rpc-usb-uart" -- -DZMK_CONFIG=/tmp/zmk-config/config -DSHIELD="anywhy_flake_left" -DZMK_EXTRA_MODULES='/__w/flake-zmk-module/flake-zmk-module'
+
+
 west build -p always \
   -d build/anywhy_flake_left \
   -s "$ZMK_LOCAL/app" \
-  -b seeeduino_xiao \
+  -b seeeduino_xiao_ble \
   -S studio-rpc-usb-uart \
   -- \
   -DZMK_CONFIG="$ZMK_CONFIG" \
   -DSHIELD_ROOT="$ZMK_CONFIG" \
   -DSHIELD=anywhy_flake_left
+
+  // use docker
+
+  docker volume create --driver local -o o=bind -o type=none \
+  -o device="/Users/naiqixiao/Documents/GitHub/flake-zmk-module/config" zmk-config
+
+  docker volume create --driver local -o o=bind -o type=none \
+  -o device="/Users/naiqixiao/Documents/GitHub/flake-zmk-module" zmk-modules
