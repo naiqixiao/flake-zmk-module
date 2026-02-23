1b0d70f705cec4644496a814ba3a4d429d5dff99
naiqixiao <naiqi.xiao@kangleelab.com>
2026-02-22 20:57:26 -0500

    feat: Implement Docker-based build process for left/right UF2 firmware, update ZMK keymap behaviors, and refine configuration settings.

--- building.sh
+++ building.sh
@@ -1,26 +1,35 @@
-export ZMK_LOCAL=/Users/naiqixiao/Documents/GitHub/zmk
-export ZMK_CONFIG=/Users/naiqixiao/Documents/GitHub/flake-zmk-module/config
+#!/bin/bash
 
-cd $ZMK_LOCAL
-source .venv/bin/activate  
+echo "Building Left Side..."
+docker run --rm -it \
+  -v "/Users/naiqixiao/Documents/GitHub/zmk:/workspaces/zmk" \
+  -v "/Users/naiqixiao/Documents/GitHub/flake-zmk-module:/workspaces/zmk/flake-zmk-module" \
+  -w /workspaces/zmk \
+  vsc-zmk-cb3b464f13040a49a608098bdd22bdc7c0bce581ee8073ce2506350e5f155305:latest \
+  west build -p always -s app -d /workspaces/zmk/flake-zmk-module/build/left -b xiao_ble//zmk -S studio-rpc-usb-uart -- \
+  -DZMK_CONFIG=/workspaces/zmk/flake-zmk-module/config \
+  -DSHIELD=anywhy_flake_left \
+  -DZMK_EXTRA_MODULES=/workspaces/zmk/flake-zmk-module \
+  -DZMK_KEYMAP=/workspaces/zmk/flake-zmk-module/config/anywhy_flake.keymap
 
-west build -s zmk/app -d "/tmp/tmp.ldrWldLPWN" -b "xiao_ble" -S "studio-rpc-usb-uart" -- -DZMK_CONFIG=/tmp/zmk-config/config -DSHIELD="anywhy_flake_left" -DZMK_EXTRA_MODULES='/__w/flake-zmk-module/flake-zmk-module'
+# Copy and rename Left UF2
+cp build/left/zephyr/zmk.uf2 anywhy_flake_left.uf2
+echo "Left Side Build Complete: anywhy_flake_left.uf2"
 
+echo "Building Right Side..."
+docker run --rm -it \
+  -v "/Users/naiqixiao/Documents/GitHub/zmk:/workspaces/zmk" \
+  -v "/Users/naiqixiao/Documents/GitHub/flake-zmk-module:/workspaces/zmk/flake-zmk-module" \
+  -w /workspaces/zmk \
+  vsc-zmk-cb3b464f13040a49a608098bdd22bdc7c0bce581ee8073ce2506350e5f155305:latest \
+  west build -p always -s app -d /workspaces/zmk/flake-zmk-module/build/right -b xiao_ble//zmk -- \
+  -DZMK_CONFIG=/workspaces/zmk/flake-zmk-module/config \
+  -DSHIELD=anywhy_flake_right \
+  -DZMK_EXTRA_MODULES=/workspaces/zmk/flake-zmk-module \
+  -DZMK_KEYMAP=/workspaces/zmk/flake-zmk-module/config/anywhy_flake.keymap
 
-west build -p always \
-  -d build/anywhy_flake_left \
-  -s "$ZMK_LOCAL/app" \
-  -b xiao_ble \
-  -S studio-rpc-usb-uart \
-  -- \
-  -DZMK_CONFIG="$ZMK_CONFIG" \
-  -DSHIELD_ROOT="$ZMK_CONFIG" \
-  -DSHIELD=anywhy_flake_left
+# Copy and rename Right UF2
+cp build/right/zephyr/zmk.uf2 anywhy_flake_right.uf2
+echo "Right Side Build Complete: anywhy_flake_right.uf2"
 
-  // use docker
-
-  docker volume create --driver local -o o=bind -o type=none \
-  -o device="/Users/naiqixiao/Documents/GitHub/flake-zmk-module/config" zmk-config
-
-  docker volume create --driver local -o o=bind -o type=none \
-  -o device="/Users/naiqixiao/Documents/GitHub/flake-zmk-module" zmk-modules
+echo "Build complete! The .uf2 files are ready in the root directory."
